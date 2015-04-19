65745d5ef76fb3c96ff46fb48cd4c88c292df382
Chris Kitching <chriskitching@linux.com>
2015-04-19 17:22:44 +0100

    Allow changing the scenario after creating the lobby

--- SCHOOK/lua/ui/maputil.lua
+++ SCHOOK/lua/ui/maputil.lua
@@ -5,3 +5,26 @@ ReallyGetArmies = GetArmies
 function GetArmies(scenario)
     return {"Player", "Coop1", "Coop2", "Coop3"}
 end
+
+-- Make the map list show coop scenarios (only)
+function EnumerateSkirmishScenarios(nameFilter, sortFunc)
+    nameFilter = nameFilter or '*'
+    sortFunc = sortFunc or DefaultScenarioSorter
+
+    -- retrieve the map file names
+    local scenFiles = DiskFindFiles('/maps', nameFilter .. '_scenario.lua')
+
+    -- load each map in to a table and store in our data structure
+    local scenarios = {}
+    for index, fileName in scenFiles do
+        local scen = LoadScenario(fileName)
+        if scen.type == "campaign_coop" then
+            table.insert(scenarios, scen)
+        end
+    end
+
+    -- sort based on name
+    table.sort(scenarios, function(a, b) return sortFunc(a.name, b.name) end)
+
+    return scenarios
+end
