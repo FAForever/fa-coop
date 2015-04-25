bd4854f8b787b47a58516f597cd3a760e4d07639
Chris Kitching <chriskitching@linux.com>
2015-04-25 07:05:02 +0100

    Don't let PlayerData use an invalid default faction

---
+++ SCHOOK/lua/ui/lobby/data/playerdata.lua
@@ -0,0 +1,3 @@
+-- Account for the fact we're busy pretending the Seraphim don't exist (hook ordering sadly makes
+-- this not be quite as rainbows and sunshine as we wanted)
+DEFAULT_MAPPING.Faction = DEFAULT_MAPPING.Faction - 1
