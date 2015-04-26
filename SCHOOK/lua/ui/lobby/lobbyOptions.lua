726baae51b6feafb710782656a9c2e8c280480c4
Chris Kitching <chriskitching@linux.com>
2015-04-26 06:00:58 +0100

    Disable more nonsensical options for coop

--- SCHOOK/lua/ui/lobby/lobbyOptions.lua
+++ SCHOOK/lua/ui/lobby/lobbyOptions.lua
@@ -7,6 +7,9 @@ function getOption(byKey, optionsTable)
     end
 end
 
+-- Note that options with only one possibility don't appear in the UI, but still find their way into
+-- the GameOptions object. Generally, this is the approach needed to delete options.
+
 -- The only victory condition option needs to be "Sandbox".
 local victoryOption = getOption("Victory", globalOpts)
 victoryOption.default = 1
@@ -27,6 +30,84 @@ spawnOption.values = {
         help = "<LOC lobui_0093>Spawn everyone in fixed locations (determined by slot)",
         key = 'fixed',
     },
+}
+
+-- Civilians are always neutral (this option actually doesn't do anything meaningful for campaign,
+-- we just don't want it in the UI.
+local civvyOption = getOption("CivilianAlliance", globalOpts)
+civvyOption.default = 1
+civvyOption.values = {
+    {
+        text = "<LOC lobui_0293>Enemy",
+        help = "<LOC lobui_0294>Civilians are enemies of players",
+        key = 'enemy',
+    }
+}
+
+-- No rush is nonsense here.
+local noRushOption = getOption("NoRushOption", globalOpts)
+noRushOption.default = 1
+noRushOption.values = {
+    {
+        text = "<LOC lobui_0318>Off",
+        help = "<LOC lobui_0319>Rules not enforced",
+        key = 'Off'
+    }
+}
+
+-- No prebuilt units (this breaks *everything*)
+local prebuiltUnitsOption = getOption("PrebuiltUnits", globalOpts)
+prebuiltUnitsOption.default = 1
+prebuiltUnitsOption.values = {
+    {
+        text = "<LOC lobui_0312>Off",
+        help = "<LOC lobui_0313>No prebuilt units",
+        key = 'Off'
+    }
+}
+
+-- Random map... Um. No.
+local randomMapOption = getOption("RandomMap", globalOpts)
+randomMapOption.default = 1
+randomMapOption.values = {
+    {
+        text = "<LOC lobui_0312>Off",
+        help = "<LOC lobui_0556>No random map",
+        key = 'Off'
+    }
+}
+
+-- Don't allow fog of war to be turned off
+local fogOption = getOption("FogOfWar", globalOpts)
+fogOption.default = 1
+fogOption.values = {
+    {
+        text = "<LOC lobui_0114>Explored",
+        help = "<LOC lobui_0115>Terrain revealed, but units still need recon data",
+        key = 'explored'
+    }
+}
+
+-- Autoteams makes no sense either...
+local autoTeamsOption = getOption("AutoTeams", teamOptions)
+autoTeamsOption.default = 1
+autoTeamsOption.values = {
+    {
+        text = "<LOC lobui_0244>None",
+        help = "<LOC lobui_0534>No automatic teams",
+        key = 'none'
+    }
+}
+
+-- Don't allow people to switch teams during the campaign...
+local teamLockOption = getOption("TeamLock", teamOptions)
+teamLockOption.default = 1
+teamLockOption.values = {
+    {
+        text = "<LOC lobui_0098>Locked",
+        help = "<LOC lobui_0099>Teams are locked once play begins",
+        key = 'locked'
+    }
 }
 
 -- Add the "Difficulty" option.
@@ -55,3 +136,6 @@ table.insert(globalOpts,
         },
     }
 )
+
+-- AI options break *everything*.
+AIOpts = {}
