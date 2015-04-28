34b5f6c0fe57f77e05786fc9527de5dc0f5c8fa2
Chris Kitching <chriskitching@linux.com>
2015-04-28 07:41:37 +0100

    A simpler way to disable restrictions

--- SCHOOK/lua/ScenarioFramework.lua
+++ SCHOOK/lua/ScenarioFramework.lua
@@ -23,4 +23,42 @@ function RemoveRestrictionForAllHumans(categories, isSilent)
     for k, armyID in ScenarioInfo.HumanPlayers do
         RemoveRestriction(armyID, categories, isSilent)
     end
+
+    PlayUnlockDialogue()
+end
+
+--- Remove a unit restriction for all human players with associated factional voiceover.
+--
+-- If the local player is of faction VOFaction, play voiceover then unlock the unit identified by
+-- categories. Otherwise just unlock the unit.
+--
+-- @param categories The unit to unlock
+-- @param VOFaction The faction the local player needs to be to hear the voiceover.
+-- @param voiceover The ID of the voiceover to play, if applicable.
+-- @param isSilent The issilent parameter to pass to RemoveRestriction.
+function UnrestrictWithVoiceover(categories, VOFaction, voiceover, isSilent)
+    if ScenarioInfo.LocalFaction == VOFaction or not VOFaction then
+        Dialogue(voiceover)
+    end
+
+    RemoveRestrictionForAllHumans(categories, isSilent)
+end
+
+--- Remove a unit restriction for all human players with associated factional voiceover and delay
+--
+-- Exactly like UnrestrictWithVoiceover, but uses CreateTimerTrigger to delay execution by the
+-- specified number of seconds.
+function UnrestrictWithVoiceoverAndDelay(cats, VOFac, delay, VO, silent)
+    -- Closure copies.
+    local categories = cats
+    local VOFaction = VOFac
+    local voiceover = VO
+    local isSilent = silent
+
+    CreateTimerTrigger(
+        function()
+            UnrestrictWithVoiceover(categories, VOFaction, voiceover, isSilent)
+        end,
+        delay
+    )
 end
