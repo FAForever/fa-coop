1ee0da3bdeeb39dbd9a495e328b82a5976e7e87d
Chris Kitching <chriskitching@linux.com>
2015-05-15 03:07:03 +0100

    Correctly initialise HumanPlayers

--- mods/coop/hook/lua/simInit.lua
+++ mods/coop/hook/lua/simInit.lua
@@ -1,29 +1,27 @@
 local MapUtil = import('/lua/ui/maputil.lua')
 
+-- The "imaginary" armies we synthesise for the coop players.
+local imaginaryArmyList = MapUtil.GetArmies()
+local imaginaryArmySet = {}
+for k, v in imaginaryArmyList do
+    imaginaryArmySet[v] = true
+end
+
+local ReallyOnCreateArmyBrain = OnCreateArmyBrain
+function OnCreateArmyBrain(index, brain, name, nickname)
+    ReallyOnCreateArmyBrain(index, brain, name, nickname)
+
+    -- Stuff this army into the HumanPlayers set, if applicable.
+    if imaginaryArmySet[name] then
+        table.insert(ScenarioInfo.HumanPlayers, index)
+    end
+end
+
 -- SetupSession loads the scenario data into a global. This is the One True Copy of scenario data.
 -- This means that now is a really good time to fart about with scenario data.
 local ReallySetupSession = SetupSession
 function SetupSession()
     ReallySetupSession()
-
-    -- Populate the HumanPlayers list in the scenario.
-    -- The maps are inconsistent about what they declare to actually be in the list.
-    -- So let's just rewrite the fucking thing ourselves.
-    local imaginaryArmyList = MapUtil.GetArmies()
-
-    -- A set to track which of the possibly-human armies are absent.
-    local absentees = {}
-
-    local newHumanPlayers = {}
-    for i, armyName in imaginaryArmyList do
-        if ScenarioInfo.ArmySetup[armyName] then
-            table.insert(newHumanPlayers, armyName)
-        else
-            absentees[armyName] = true
-        end
-    end
-
-    ScenarioInfo.HumanPlayers = newHumanPlayers
 
     -- Erase alliances that refer to armies that we don't have players for.
     -- This shouldn't really be necessary, but things catch fire trying to create alliances to
@@ -32,7 +30,7 @@ function SetupSession()
         if army.Alliances then
             local newAlliances = {}
             for allianceeName, allianceType in army.Alliances do
-                if not absentees[allianceeName] then
+                if ScenarioInfo.ArmySetup[allianceeName] then
                     newAlliances[allianceeName] = allianceType
                 else
                     WARN("Dropping alliance from " .. k .. " to " .. allianceeName .. " of type " .. allianceType)
