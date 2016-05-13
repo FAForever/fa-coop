2e9916a20ca015e6517cbf7a1b7ed6e79bd8997b
speed2CZ <GTGTGT@seznam.cz>
2016-05-13 11:40:42 +0200

    Add Generic Reminders for Vanilla
    These are used in some vanilla missions

---
+++ mods/coop/hook/lua/ScenarioStrings.lua
@@ -0,0 +1,12 @@
+--------------------
+-- Generic Reminders
+--------------------
+UEFGenericReminder = {
+  {text = '<LOC E01_M01_060_010>[{i EarthCom}]: Sir, maybe you should check your objectives.', vid = 'E01_EarthCom_M01_01131.sfd', bank = 'E01_VO', cue = 'E01_EarthCom_M01_01131', faction = 'UEF'},
+}
+CybranGenericReminder = {
+  {text = '<LOC C01_M01_060_010>[{i Ops}]: I would suggest you check your objectives, Commander. Ops out.', vid = 'C01_Ops_M01_01536.sfd', bank = 'C01_VO', cue = 'C01_Ops_M01_01536', faction = 'Cybran'},
+}
+AeonGenericReminder = {
+  {text = '<LOC A01_M01_060_010>[{i Choir}]: Check your objectives if necessary, Commander. Choir out.', vid = 'A01_Choir_M01_01557.sfd', bank = 'A01_VO', cue = 'A01_Choir_M01_01557', faction = 'Aeon'},
+}
