b458a36508a55f1353a6ba0bcf92c898d5370d6f
KionX <5648030+KionX@users.noreply.github.com>
2019-12-03 22:18:04 +0500

    code refactoring

--- mods/coop/hook/lua/sim/ScenarioUtilities.lua
+++ mods/coop/hook/lua/sim/ScenarioUtilities.lua
@@ -63,21 +63,15 @@ end
 ----[                                                                             ]--
 function InitializeScenarioArmies()
     isCommonArmy = ScenarioInfo.Options.CommonArmy == 'true'
-
-    local armyPlayers = {}
-    local humansIndex = 1
-    for _, army in ScenarioInfo.ArmySetup do
-        if not army.Human then continue end
-        armyPlayers[army.ArmyIndex] = humansIndex
-        humansIndex = humansIndex + 1
-    end
     
     if isCommonArmy then
+        local humansIndex = 0
         for strArmy, iArmy in ScenarioInfo.HumanPlayers do
-            local selHumansIndex = armyPlayers[iArmy]
-            if (strArmy == 'Player1') or (selHumansIndex == nil) then continue end
-            ArmyGetHandicap(ScenarioInfo.HumanPlayers['Player1'] - 1, selHumansIndex - 1, true)
-            ArmyGetHandicap(iArmy - 1, selHumansIndex - 1, false)
+            if GetArmyBrain(iArmy).BrainType ~= 'Human' then continue end
+            humansIndex = humansIndex + 1
+            if strArmy == 'Player1' then continue end
+            ArmyGetHandicap(ScenarioInfo.HumanPlayers['Player1'] - 1, humansIndex - 1, true)
+            ArmyGetHandicap(iArmy - 1, humansIndex - 1, false)
             if GetFocusArmy() == iArmy then
                 ForkThread(
                 function(leaderIndex)
