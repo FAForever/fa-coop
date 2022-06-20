29a72d659133a368cf727f77bb9dcf27f816bd84
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2022-06-20 20:56:02 +0200

    Fixes the movie files for the base game missions

--- mods/coop/hook/lua/ui/dialogs/mapselect.lua
+++ mods/coop/hook/lua/ui/dialogs/mapselect.lua
@@ -19,6 +19,7 @@ end
 
 local reallyCreateDialog = CreateDialog
 function CreateDialog(...)
+    LOG(" I HAPPENED!")
     local ret = reallyCreateDialog(unpack(arg))
 
     -- Delete the map filter UI (there aren't enough coop missions to warrant a scrollbar, and the most
