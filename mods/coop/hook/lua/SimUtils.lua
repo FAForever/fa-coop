c564364608d773dee16c6bced1b1b7be14a82a1d
Jip <w.b.wijnia@gmail.com>
2022-11-26 20:21:46 +0100

    Styling of code

--- mods/coop/hook/lua/SimUtils.lua
+++ mods/coop/hook/lua/SimUtils.lua
@@ -2,7 +2,10 @@ do
     --- Filter out units that were set in the script that they can't be given to other players
     local oldTransferUnitsOwnership = TransferUnitsOwnership
     TransferUnitsOwnership = function(units, ToArmyIndex)
-        if not units then return end
+        if not units then
+            return
+        end
+
         local toGiveUnits = {}
         for _, v in units do
             if v.CanBeGiven then
